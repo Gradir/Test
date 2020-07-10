d89d8bff466b84d0f2bfb648c05d92a4c316c25c
Piotr Kolodziejczyk <piotr.kolodziejczyk@swiftapps.pl>
2020-07-11 00:05:27 +0200

    Initial Commit

---
+++ Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by SwiftApps on 11/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
